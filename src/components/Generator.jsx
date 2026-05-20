ad018acb3dd4c9117131fc2c9708c31843c41a8f
adhiiiiiii <adilmhd8848@gmail.com>
2026-05-20 16:33:54 +0530

    Complete workout generator app UI and functionality

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -19,12 +19,11 @@ function Header(props) {
     )
 }
 
-export default function Generator() {
+export default function Generator(props) {
+
+    const { poison, setPoison, muscles, setMuscles, goal, setGoal, updateWorkout } = props
 
     const [ showModal,setShowModal ] = useState(false)
-    const [ poison,setPoison ] = useState('individual')
-    const [ muscles , setMuscles ] = useState([])
-    const [ goal , setGoal ] = useState('strength_power')
 
     // let showModal = false
 
@@ -53,7 +52,7 @@ export default function Generator() {
     }
 
     return (
-    <SectionWrapper header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}>
+    <SectionWrapper id={"generate"} header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}>
         <Header index={'01'} title={'Pick your poison'} description={"Select the workout you wish to endure."} />
         <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
            {Object.keys(WORKOUTS).map((type, typeIndex) => {
@@ -92,7 +91,7 @@ export default function Generator() {
         </div>
 
         <Header index={'03'} title={'Become Juggernaut'} description={"Select your ultimate objective."} />
-        <div className='grid grid-cols-3 gap-4'>
+        <div className='grid grid-col-1 sm:grid-cols-3 gap-4'>
            {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
              return (
               <button onClick={ () => { 
@@ -106,7 +105,7 @@ export default function Generator() {
 
         </div>
 
-        <Button text = {'Formulate'}></Button>
+        <Button func={updateWorkout} text = {'Formulate'}></Button>
     </SectionWrapper>
   )
 }
